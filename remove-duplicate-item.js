1882b6b43778c204401a6483fd0afe79a5dfa468
kabir-hussan <kabirhussan891@gmail.com>
2024-02-07 00:18:28 +0600

    final array without duplicate item done

--- remove-duplicate-item.js
+++ remove-duplicate-item.js
@@ -2,3 +2,23 @@
  * Remove duplicate item from an array
  */
 
+function noDuplicate(array){
+
+    const uniqueArray= [];
+
+    for(const item of array)
+    {
+        if(uniqueArray.includes(item) === false)
+        {
+            uniqueArray.push(item);
+        }
+    }
+
+    return uniqueArray;
+}
+
+const duplicateArray= ['kabir', 'nahid', 'fahim', 'arif', 'kabir', 'maruf', 'nahid'];
+
+const finalArray= noDuplicate(duplicateArray);
+
+console.log(finalArray);
